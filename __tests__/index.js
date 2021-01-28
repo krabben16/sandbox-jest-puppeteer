d87db7b9aeabf8e74cc0a322456c117922b694d1
Hiroki Kawaguchi <kawakanu@gmail.com>
2021-01-28 21:57:42 +0900

    add: selector test case

--- __tests__/index.js
+++ __tests__/index.js
@@ -2,10 +2,24 @@
 
 describe('Google', () => {
   beforeAll(async () => {
-    await page.goto('https://google.com')
+    await page.goto('https://www.google.co.jp')
   })
 
   test('should be titled "Google"', async () => {
     await expect(page.title()).resolves.toMatch('Google')
+  })
+
+  test('selector test1', async () => {
+    const selector = '#hptl > a:nth-child(1)'
+    const item = await page.$(selector)
+    expect(item.getProperty('innerText')).toMatch('Googleについて')
+  })
+
+  test('selector test2', async () => {
+    const selector = '#hptl > a:nth-child(1)'
+    const data = await page.$eval(selector, item => {
+      return item.innerText
+    })
+    expect(data).toMatch('Googleについて')
   })
 })
